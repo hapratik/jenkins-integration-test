d1895d336cb6298455bd1a67f7c5cd688fd426e5
hapratik <113336601+hapratik@users.noreply.github.com>
2022-12-14 22:34:04 +0530

    Create automate.sh

---
+++ automate.sh
@@ -0,0 +1,21 @@
+#all_branches
+branch_names=$(git branch -r | cut -c 10- | tail -n +2)
+
+#arr=$(git branch -r | cut -c 10- | tail -n +2)
+for branch in $branch_names
+do
+echo $branch
+if [ "$branch" = "production" ]; then 
+    #count=$(git tag -l |wc -l)
+    #tag_arr=$(git tag -l)
+    echo "production branch"
+    #docker build -t nest-sample-app:$tag
+    docker build -t nest-sample-app:$tag_arr
+    break
+else
+    echo "other branch"
+    docker build -t nest-sample-app:$branch
+    sleep 5
+fi;
+echo "1"
+done
